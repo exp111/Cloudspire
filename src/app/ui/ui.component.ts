631eada960ac57d50a14ea316fac729b12063388
Exp <17885980+exp111@users.noreply.github.com>
2025-10-18 21:54:36 +0200

    bootstrap

--- src/app/ui/ui.component.ts
+++ src/app/ui/ui.component.ts
@@ -1,13 +1,17 @@
 import {Component} from '@angular/core';
+import {NgbDropdown, NgbDropdownItem, NgbDropdownMenu, NgbDropdownToggle} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-ui',
   standalone: true,
   imports: [
+    NgbDropdownToggle,
+    NgbDropdown,
+    NgbDropdownMenu,
+    NgbDropdownItem
   ],
   templateUrl: './ui.component.html',
   styleUrl: './ui.component.scss'
 })
 export class UiComponent {
-
 }
