c09e0bf067dd0194ff1530572d9484d56426879f
Exp <17885980+exp111@users.noreply.github.com>
2025-10-18 17:29:42 +0200

    grovetender data

--- src/data/grovetender.ts
+++ src/data/grovetender.ts
@@ -1,5 +1,5 @@
 import {FactionData} from "./model/faction";
-import {HeroData, SpireData} from "./model/chip";
+import {HeroData, MinionData, SpireData} from "./model/chip";
 import {FactionType, Terrain} from "./enums";
 
 class GrovetendersHeroData extends HeroData {
@@ -7,6 +7,10 @@ class GrovetendersHeroData extends HeroData {
 }
 
 class GrovetendersSpireData extends SpireData {
+  override faction = FactionType.GROVETENDERS;
+}
+
+class GrovetendersMinionData extends MinionData {
   override faction = FactionType.GROVETENDERS;
 }
 
@@ -25,10 +29,99 @@ export class GrovetendersFactionData implements FactionData {
       allowance: Terrain.Forest,
       promotedHealth: 3
     }),
-    //TODO: other grovetender heroes
+    new GrovetendersHeroData({
+      name: "Ybanthe",
+      cost: 4,
+      reward: 2,
+      health: 5,
+      attack: 1,
+      movement: 2,
+      capacity: 2,
+      allowance: Terrain.Forest,
+      promotedAttack: 2,
+      promotedMovement: 3,
+      promotedReward: 3
+    }),
+    new GrovetendersHeroData({
+      name: "Wyvankaye",
+      cost: 7,
+      reward: 0,
+      health: 2,
+      attack: 0,
+      movement: 3,
+      capacity: 0,
+      allowance: Terrain.Forest,
+      promotedMovement: 4,
+      promotedAllowance: Terrain.Mountain
+    }),
   ];
   minions = [
-    //TODO: grovetender minions
+    new GrovetendersMinionData({
+      name: "War Briar",
+      amount: 3,
+      cost: 2,
+      reward: 1,
+      health: 3,
+      attack: 1,
+      movement: 2,
+      promotedHealth: 4,
+      promotedAttack: 2
+    }),
+    new GrovetendersMinionData({
+      name: "Vineherald",
+      amount: 2,
+      cost: 3,
+      reward: 1,
+      health: 3,
+      attack: 1,
+      movement: 2,
+      promotedHealth: 4
+    }),
+    new GrovetendersMinionData({
+      name: "Taproot",
+      amount: 3,
+      cost: 3,
+      reward: 2,
+      health: 3,
+      attack: 1,
+      movement: 2,
+      promotedHealth: 1,
+      promotedMovement: 1
+    }),
+    new GrovetendersMinionData({
+      name: "Treed",
+      amount: 2,
+      cost: 3,
+      reward: 2,
+      health: 4,
+      attack: 1,
+      movement: 2,
+      promotedHealth: 5,
+      promotedAttack: 2,
+      promotedMovement: 3,
+      promotedAllowance: Terrain.Forest,
+    }),
+    new GrovetendersMinionData({
+      name: "Ogregrowth",
+      cost: 5,
+      reward: 3,
+      health: 5,
+      attack: 2,
+      movement: 2,
+      promotedHealth: 6,
+      promotedAttack: 3
+    }),
+    new GrovetendersMinionData({
+      name: "Grizzled Oak",
+      cost: 6,
+      reward: 3,
+      health: 6,
+      attack: 2,
+      movement: 1,
+      promotedHealth: 7,
+      promotedAttack: 4,
+      promotedMovement: 2
+    }),
   ];
   spires = [
     new GrovetendersSpireData({
@@ -49,7 +142,26 @@ export class GrovetendersFactionData implements FactionData {
       range: 1,
       capacity: 3
     }),
-    //TODO: other grovetender spires
+    new GrovetendersSpireData({
+      name: "Creeping Vines",
+      //TODO: promoted version of shrubbery
+      amount: 4,
+      cost: 4,
+      reward: 2,
+      attack: 1,
+      fortification: 1,
+      capacity: 3
+    }),
+    new GrovetendersSpireData({
+      name: "Muskeg",
+      //TODO: promoted version of reetall
+      amount: 4,
+      cost: 5,
+      reward: 3,
+      attack: 1,
+      range: 1,
+      capacity: 3
+    }),
   ];
   chips = [
     ...this.heroes,
--- src/data/model/chip.ts
+++ src/data/model/chip.ts
@@ -65,9 +65,23 @@ export class HeroData extends ChipData {
 
 export class MinionData extends ChipData {
   health!: number;
+  attack!: number;
+  movement!: number;
+  allowance!: Terrain;
+  promotedReward!: number;
+  promotedHealth!: number;
+  promotedAttack!: number;
+  promotedMovement!: number;
+  promotedAllowance!: Terrain;
 
   constructor(data: Partial<MinionData>) {
     super(data);
     this.health = this.health ?? 0;
+    this.allowance = this.allowance ?? Terrain.Grass;
+    this.promotedReward = this.promotedReward ?? this.reward;
+    this.promotedHealth = this.promotedHealth ?? this.health;
+    this.promotedAttack = this.promotedAttack ?? this.attack;
+    this.promotedMovement = this.promotedMovement ?? this.movement;
+    this.promotedAllowance = this.promotedAllowance ?? this.allowance;
   }
 }
